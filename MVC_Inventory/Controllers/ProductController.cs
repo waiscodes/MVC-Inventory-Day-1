26e26160569cd12ab1f223d53e8cf79da2a65750
waiscodes <b.wais@outlook.com>
2020-11-09 15:07:18 -0700

    added business logic on create for name

--- MVC_Inventory/Controllers/ProductController.cs
+++ MVC_Inventory/Controllers/ProductController.cs
@@ -16,7 +16,32 @@ namespace MVC_Inventory.Controllers
 
         public Product CreateProduct(string name, string quantity)
         {
+            name = !string.IsNullOrWhiteSpace(name) ? name.Trim() : null;
+            quantity = !string.IsNullOrWhiteSpace(quantity) ? quantity.Trim() : null;
+            int dfltQuant = 0;
+            int parsedQuant;
 
+            using (InventoryContext context = new InventoryContext())
+            {
+
+                if (string.IsNullOrWhiteSpace(name))
+                {
+                    throw new Exception("Name cannot be null");
+                }
+                else if (name.Length > 30)
+                {
+                    throw new Exception("Name must be under 30 characters");
+                }
+                else if (context.Products.Any(x => x.Name.ToLower() == name.ToLower()))
+                {
+                    throw new Exception("This product already exists");
+                }
+
+                if(string.IsNullOrWhiteSpace(quantity))
+                {
+
+                }
+            }
         }
 
         //public Product DiscontinueProductByID(string id)
