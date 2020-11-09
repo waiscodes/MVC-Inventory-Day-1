4e9c7197bc0a8fa5fa5b1ba3a2d49abc9800f8c6
waiscodes <b.wais@outlook.com>
2020-11-09 15:47:21 -0700

    added package for json formatting

---
+++ MVC_Inventory/Controllers/InventoryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Inventory.Controllers;
+using MVC_Inventory.Models;
+
+namespace ProductInformation.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductAPIController : ControllerBase
+    {
+        [HttpPost("Create")]
+        public ActionResult<Product> ProductCreate_POST(string name, string quantity)
+        {
+            ActionResult<Product> result;
+            try
+            {
+                result = new ProductController().CreateProduct(name, quantity);
+            }
+            catch
+            {
+                result = StatusCode(500, "Unknown error occurred, please try again later.");
+            }
+            return result;
+        }
+    }
+}
